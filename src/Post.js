6c010fbdd620f593e0f8c8447ba523fd675d9692
Haripriya <haripriya@psyberhealth.com>
2024-05-06 16:48:09 +0530

    removed readmore and formatted css

--- src/Post.js
+++ src/Post.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import parse from 'html-react-parser'; // Import parse function to parse HTML content
-import './Post.css'; // Import CSS for styling
+import parse from 'html-react-parser';
+import './Post.css';
 
 const Post = () => {
   const [postContent, setPostContent] = useState('');
   const [postTitle, setPostTitle] = useState('');
   const [postDate, setPostDate] = useState('');
   const [postCategory, setPostCategory] = useState('');
+
   const location = useLocation();
 
   useEffect(() => {
@@ -15,10 +16,10 @@ const Post = () => {
       const searchParams = new URLSearchParams(location.search);
       const content = searchParams.get('content');
       const title = searchParams.get('title');
-      const postDate = searchParams.get('pubDate');
-      const postCategory = searchParams.get('category');
-      setPostDate(postDate || '');
-      setPostCategory(postCategory || '');
+      const date = searchParams.get('pubDate');
+      const category = searchParams.get('category');
+      setPostDate(date || '');
+      setPostCategory(category || '');
       setPostContent(content || '');
       setPostTitle(title || '');
     };
@@ -32,61 +33,14 @@ const Post = () => {
     return date.toLocaleDateString('en-US', options);
   };
 
-  const renderGallery = (content) => {
-    const contentStr = content.toString(); // Convert content to a string
-
-    // Split the content into separate images and titles
-    const images = contentStr.match(/<img[^>]+>/g) || [];
-
-    // Check if there are more than four images in a row
-    if (images.length > 3) {
-      const galleryItems = [];
-      let imageCount = 0;
-      let galleryRow = [];
-
-      // Group images into rows of four
-      images.forEach((image, index) => {
-        galleryRow.push(parse(image));
-        imageCount++;
-
-        // If four images are added to the row or it's the last image, add the row to galleryItems
-        if (imageCount === 2 || index === images.length - 1) {
-          galleryItems.push(
-            <div key={index} className="gallery-row">
-              {galleryRow.map((item, idx) => (
-                <div key={idx} className="gallery-item">{item}</div>
-              ))}
-            </div>
-          );
-          galleryRow = [];
-          imageCount = 0;
-        }
-      });
-
-      return (
-        <div className="gallery">{galleryItems}</div>
-      );
-    } else {
-      // Render images without gallery
-      return parse(content); // Parse content as usual
-    }
-  };
-
   return (
     <div className="post-container">
       <h1 className="post-title">{parse(postTitle)}</h1>
-      <p className="post-date">
-        {parse(formatDate(postDate))}
-        {postCategory && (
-          <>
-            {' | '}
-            {parse(postCategory)}
-          </>
-        )}
-      </p>
-      <div className="post-content">
-        {renderGallery(postContent)}
+      <div className="post-meta">
+        <span className="author-name">Haripriya Sridharan</span> &bull;
+        <span className="post-date">{parse(formatDate(postDate))}</span>
       </div>
+      <div className="post-content">{parse(postContent)}</div>
     </div>
   );
 };
--- src/RSSFeed.js
+++ src/RSSFeed.js
@@ -6,55 +6,44 @@ const targetUrl = 'https://a4c5-106-51-78-16.ngrok-free.app/rss-feed';
 
 const RSSFeed = () => {
   const [feedItems, setFeedItems] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState(''); // State for selected category
-  const [filteredFeedItems, setFilteredFeedItems] = useState([]); // State for filtered feed items
-  const [loading, setLoading] = useState(true); // State for loading indicator
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [filteredFeedItems, setFilteredFeedItems] = useState([]);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchRSSFeed = async () => {
       try {
-        const response = await fetch(targetUrl, {
-          method: 'GET',
-        });
+        const response = await fetch(targetUrl);
         const data = await response.text();
         const parser = new DOMParser();
         const xmlDoc = parser.parseFromString(data, 'text/xml');
         const items = xmlDoc.querySelectorAll('item');
-        const parsedItems = Array.from(items).map(item => {
-            const enclosureElement = item.querySelector('enclosure');
-
-            const enclosureUrl = enclosureElement ? enclosureElement.getAttribute('url') : null;
-
-          const newItem = {
-            title: item.querySelector('title').textContent,
-            link: item.querySelector('link').textContent,
-            pubDate: item.querySelector('pubDate').textContent,
-            description: item.querySelector('description').textContent,
-            category: item.querySelector('category') ? item.querySelector('category').textContent : '',
-            content: item.querySelector('content\\:encoded, encoded').textContent,
-            enclosure: enclosureUrl, // Extract enclosure URL
-        };
-          return newItem;
-        });
+        const parsedItems = Array.from(items).map(item => ({
+          title: item.querySelector('title').textContent,
+          link: item.querySelector('link').textContent,
+          pubDate: item.querySelector('pubDate').textContent,
+          description: item.querySelector('description').textContent,
+          category: item.querySelector('category') ? item.querySelector('category').textContent : '',
+          content: item.querySelector('content\\:encoded, encoded').textContent,
+          enclosure: item.querySelector('enclosure') ? item.querySelector('enclosure').getAttribute('url') : null
+        }));
         setFeedItems(parsedItems);
-        setLoading(false); // Set loading to false when data is fetched
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching RSS feed:', error);
-        setLoading(false); // Set loading to false in case of error
+        setLoading(false);
       }
     };
-
     fetchRSSFeed();
   }, []);
 
   useEffect(() => {
-    // Filter feed items based on selected category
     if (selectedCategory) {
       const filteredItems = feedItems.filter(item => item.category === selectedCategory);
       setFilteredFeedItems(filteredItems);
     } else {
-      setFilteredFeedItems(feedItems); // If no category selected, display all feed items
+      setFilteredFeedItems(feedItems);
     }
   }, [selectedCategory, feedItems]);
 
@@ -69,35 +58,29 @@ const RSSFeed = () => {
         <label htmlFor="category">Category</label>
         <select id="category" value={selectedCategory} onChange={e => setSelectedCategory(e.target.value)}>
           <option value="">All</option>
-          {/* Map through unique categories and create dropdown options */}
           {[...new Set(feedItems.map(item => item.category))].map((category, index) => (
             <option key={index} value={category}>{category}</option>
           ))}
         </select>
       </div>
       {loading ? (
-         <div className="loader">
-       </div> // Render loading indicator while data is being fetched
+        <div className="loader"></div>
       ) : (
         <ul className="rss-feed-list">
-          {/* Render filtered feed items */}
           {filteredFeedItems.map((item, index) => (
             <li key={index} className="rss-feed-item">
               <div className="rss-feed-item-image">
                 {item.enclosure && (
-                  <img src={item.enclosure} alt="Enclosure" className="enclosure-image" />
+                  <img src={item.enclosure} alt="Enclosure" className="enclosure-image" onClick={() => handleReadMore(item.content, item.title, item.pubDate, item.category)}/>
                 )}
               </div>
               <div className="rss-feed-item-content">
-                <h2 className="rss-feed-item-title" onClick={() => handleReadMore(item.content, item.title, item.pubDate, item.category)}>{item.title}</h2>
+              <h2 className="rss-feed-item-title" onClick={() => handleReadMore(item.content, item.title, item.pubDate, item.category)}>{item.title}</h2>
                 <p className="rss-feed-item-description">{item.description}</p>
-                <p className="rss-feed-item-date">Date: {item.pubDate}</p>
-                <button
-                  className="rss-feed-item-button"
-                  onClick={() => handleReadMore(item.content, item.title, item.pubDate, item.category)}
-                >
+                <p className="rss-feed-item-date">Date: {new Date(item.pubDate).toLocaleDateString()}</p>
+                {/* <button className="rss-feed-item-button" onClick={() => handleReadMore(item.content, item.title, item.pubDate, item.category)}>
                   Read more
-                </button>
+                </button> */}
               </div>
             </li>
           ))}
