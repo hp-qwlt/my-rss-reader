a96d43d2982365cb5fe49b4c51609e60e9e639f7
Haripriya <haripriya@psyberhealth.com>
2024-05-06 22:18:48 +0530

    updated styles and integrated api

--- src/Post.js
+++ src/Post.js
@@ -8,6 +8,7 @@ const Post = () => {
   const [postTitle, setPostTitle] = useState('');
   const [postDate, setPostDate] = useState('');
   const [postCategory, setPostCategory] = useState('');
+  const [likesCount, setLikesCount] = useState('');
 
   const location = useLocation();
 
@@ -18,8 +19,10 @@ const Post = () => {
       const title = searchParams.get('title');
       const date = searchParams.get('pubDate');
       const category = searchParams.get('category');
+      const likesCount = searchParams.get('likesCount');
+      console.log('likesData',likesCount)
       const cleanedContent = content ? content.replace(/(<br\s*\/?>\s*){2,}/gi, '<br>').replace(/(<br>\s*<\/(ul|p|li)>)/gi, '</$2>') : '';
-      
+      setLikesCount(likesCount || '')
       setPostDate(date || '');
       setPostCategory(category || '');
       setPostContent(cleanedContent || '');
@@ -39,11 +42,25 @@ const Post = () => {
     <div className="post-container">
       <h1 className="post-title">{parse(postTitle)}</h1>
       <div className="post-meta">
-        <span className="author-name">Haripriya Sridharan</span> &bull;
-        <span className="post-date">{parse(formatDate(postDate))}</span>
+        <span className="author-name">Haripriya Sridharan</span> &bull;&nbsp;
+        <span className="post-date">{parse(formatDate(postDate))}</span> 
         <hr/>
       </div>
       <div className="post-content">{parse(postContent)}</div>
+      {likesCount ? (
+            <span>
+              <svg stroke="currentColor" fill="currentColor" strokeWidth="2" viewBox="0 0 24 24" color="black" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{ color: 'black', fill: 'white', stroke: 'red' }}>
+                <path style={{display:'none'}} fill="none" d="M0 0h24v24H0z"></path>
+                <path d="m12 21.35-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path>
+              </svg>
+              &nbsp;
+              <span>{likesCount}</span>
+            </span>
+          ) : (
+            <div id="heart">
+              <img className="bottom" src="https://images.freeimages.com/image/previews/a7f/pink-love-heart-png-design-5692900.png" width="20px" />
+            </div>
+          )}
     </div>
   );
 };
--- src/RSSFeed.js
+++ src/RSSFeed.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { RiLoader2Line } from 'react-icons/ri'; // Import loader icon
+import { MdHome, MdFavorite } from 'react-icons/md'; // Import Material Design home and favorite icons
 import './RSSFeed.css'; // Import CSS for styling
-import { MdHome } from 'react-icons/md'; // Import Material Design home icon
 
 const targetUrl = 'https://a4c5-106-51-78-16.ngrok-free.app/rss-feed';
+const targetUrlScrape = 'https://a4c5-106-51-78-16.ngrok-free.app/scrape';
 
 const RSSFeed = () => {
   const [feedItems, setFeedItems] = useState([]);
+  const [likesData, setLikesData] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('');
   const [filteredFeedItems, setFilteredFeedItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [likesLoading, setLikesLoading] = useState(true);
+  const [likedItems, setLikedItems] = useState([]); // State to keep track of liked items
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -21,7 +25,7 @@ const RSSFeed = () => {
         const parser = new DOMParser();
         const xmlDoc = parser.parseFromString(data, 'text/xml');
         const items = xmlDoc.querySelectorAll('item');
-        const parsedItems = Array.from(items).map(item => ({
+        const rssItems = Array.from(items).map(item => ({
           title: item.querySelector('title').textContent,
           link: item.querySelector('link').textContent,
           pubDate: item.querySelector('pubDate').textContent,
@@ -30,10 +34,11 @@ const RSSFeed = () => {
           content: item.querySelector('content\\:encoded, encoded').textContent,
           enclosure: item.querySelector('enclosure') ? item.querySelector('enclosure').getAttribute('url') : null,
         }));
-        setFeedItems(parsedItems);
+
+        setFeedItems(rssItems);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching RSS feed:', error);
+        console.error('Error fetching data:', error);
         setLoading(false);
       }
     };
@@ -49,25 +54,49 @@ const RSSFeed = () => {
     }
   }, [selectedCategory, feedItems]);
 
-  const handleReadMore = (content, title, pubDate, category) => {
-    navigate(`/post?pubDate=${encodeURIComponent(pubDate)}&category=${encodeURIComponent(category)}&title=${encodeURIComponent(title)}&content=${encodeURIComponent(content)}`);
-  };
+  useEffect(() => {
+    const fetchLikesData = async () => {
+      try {
+        const response = await fetch(targetUrlScrape);
+        const data = await response.json();
+        setLikesData(data);
+        setLikesLoading(false);
+      } catch (error) {
+        console.error('Error fetching likes data:', error);
+        setLikesLoading(false);
+      }
+    };
+    fetchLikesData();
+  }, []);
 
+  const handleReadMore = (content, title, pubDate, category,likesCount) => {
+    navigate(`/post?likesCount=${encodeURIComponent(likesCount)}&pubDate=${encodeURIComponent(pubDate)}&category=${encodeURIComponent(category)}&title=${encodeURIComponent(title)}&content=${encodeURIComponent(content)}`);
+  };
+  
   const handleImageLoad = (index) => {
     const updatedFeedItems = [...feedItems];
     updatedFeedItems[index].imageLoading = false;
     setFeedItems(updatedFeedItems);
   };
 
+  const handleLikeToggle = (title) => {
+    setLikedItems(prevLikedItems => {
+      if (prevLikedItems.includes(title)) {
+        return prevLikedItems.filter(item => item !== title);
+      } else {
+        return [...prevLikedItems, title];
+      }
+    });
+  };
+
   return (
     <div className="rss-feed">
-      {/* Navigation Menu */}
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', background: '#fff' }}>
         <nav className="rss-nav">
           <a href="https://haripriya.org" className="nav-home"><MdHome color= '#35495E' size='3em' /></a>
         </nav>
-        <h1 className="rss-feed-title" style={{ flexGrow: 1, textAlign: 'center' }}>Posts</h1>
-        <div style={{ width: '50px' }}> {/* Placeholder to balance the layout */}</div>
+        <h1 className="rss-feed-title">Posts</h1>
+        <div style={{ width: '50px' }}></div> {/* Placeholder for balance */}
       </div>
       <div className="category-dropdown">
         <label htmlFor="category">Category</label>
@@ -79,7 +108,7 @@ const RSSFeed = () => {
         </select>
       </div>
       {loading ? (
-        <div className="loader"></div>
+        <div className="loader"><RiLoader2Line /></div>
       ) : (
         <ul className="rss-feed-list">
           {filteredFeedItems.map((item, index) => (
@@ -87,21 +116,48 @@ const RSSFeed = () => {
               <div className="rss-feed-item-image">
                 {item.enclosure && (
                   <>
-                    {item.imageLoading && <RiLoader2Line className="loader-icon" />}
                     <img
                       src={item.enclosure}
                       alt="Enclosure"
                       className={`enclosure-image ${item.imageLoading ? 'hide' : ''}`}
                       onLoad={() => handleImageLoad(index)}
-                      onClick={() => handleReadMore(item.content, item.title, item.pubDate, item.category)}
+                      onClick={() => {
+                        const postLikesData = likesData.find(like => like.title === item.title);
+                        const likesCount = postLikesData ? postLikesData.likesCount : 0;
+                        handleReadMore(item.content, item.title, item.pubDate, item.category, likesCount)}
+                      }
                     />
                   </>
                 )}
               </div>
               <div className="rss-feed-item-content">
-                <h2 className="rss-feed-item-title" onClick={() => handleReadMore(item.content, item.title, item.pubDate, item.category)}>{item.title}</h2>
+                <h2 className="rss-feed-item-title" onClick={() => {
+                  const postLikesData = likesData.find(like => like.title === item.title);
+                  const likesCount = postLikesData ? postLikesData.likesCount : 0;
+                  handleReadMore(item.content, item.title, item.pubDate, item.category, likesCount)}
+                }>{item.title}</h2>
                 <p className="rss-feed-item-description">{item.description}</p>
                 <p className="rss-feed-item-date">Date: {new Date(item.pubDate).toLocaleDateString()}</p>
+                <span onClick={() => handleLikeToggle(item.title)}>
+                <span className="favorite-icon">
+                  
+                  {likesData.find(like => like.title === item.title) ? (
+                    <span>
+                    <svg stroke="currentColor" fill="currentColor" strokeWidth="2" viewBox="0 0 24 24" color="black" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{ color: 'black', fill: likedItems.includes(item.title) ? 'red' : 'white', stroke: likedItems.includes(item.title) ? 'none' : 'red' }}>
+                      <path style={{display:'none'}} fill="none" d="M0 0h24v24H0z"></path>
+                      <path d="m12 21.35-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path>
+                     </svg>
+                     &nbsp;
+                    <span>{likesData.find(like => like.title === item.title).likesCount}</span>
+                    </span>
+                  ) : (
+                    <div id="heart">
+                                          <img class="bottom" src="https://images.freeimages.com/image/previews/a7f/pink-love-heart-png-design-5692900.png" width="20px" />
+                    </div>
+                  )}
+                </span>
+              </span>
+
               </div>
             </li>
           ))}
